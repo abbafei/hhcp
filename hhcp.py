451702373dee80458979d015a737b792c5e51600
yossi <yossi@yy-pc>
2014-05-28 07:16:17 -0400

    B"H hcp - repair -k (keep listening after one request) mode

--- hhcp.py
+++ hhcp.py
@@ -89,8 +89,8 @@ def zcat_iter(ii):
         return
 
 
-def open_output_file_fd(di_file):
-    return (os.fdopen(sys.stdout.fileno(), 'wb') if (di_file is None) else open(di_file, 'ab'))
+def open_file_fd(di_file, is_writing=False):
+    return (os.fdopen((sys.stdout if is_writing else sys.stdin).fileno(), ('wb' if is_writing else 'rb')) if (di_file is None) else open(di_file, ('ab' if is_writing else 'rb')))
 
 
 
@@ -102,6 +102,7 @@ def hcp_state(di_file=None, keep_listening=False, return_status=None, type_read=
         rs = return_status
     else:
         rs = '200 ok Boruch Hashem'
+    fw = di_file
     def handle_http_request(environ, start_response):
         if state['done']:
             return
@@ -138,20 +139,19 @@ def hcp_state(di_file=None, keep_listening=False, return_status=None, type_read=
                 fro = environ['wsgi.input']
                 ced = {'gzip': zcat_iter}
                 fr = (ctf[content_type](fs=cgi.FieldStorage(fp=fro, environ=environ), field_name=field_name) if (content_type in ctf) else fro)
-                with open_output_file_fd(di_file) as fw:
-                    try:
-                        foi = read_post_fileobj(fr, content_length)
-                        foidec = (ced[environ['HTTP_CONTENT_ENCODING']] if (('HTTP_CONTENT_ENCODING' in environ) and (environ['HTTP_CONTENT_ENCODING'] in ced)) else (lambda ii: ii))(foi)
-                        for c in foidec:
-                            fw.write(c)
-                    except IOError as e:
-                        if e.errno in (9, 32):
-                            sys.stderr.write('{0}\n'.format(str(e)))
-                            state['exit'] = 2
-                        else:
-                            raise
-                    if raw_format:
-                        yield rcvd_msg_body
+                try:
+                    foi = read_post_fileobj(fr, content_length)
+                    foidec = (ced[environ['HTTP_CONTENT_ENCODING']] if (('HTTP_CONTENT_ENCODING' in environ) and (environ['HTTP_CONTENT_ENCODING'] in ced)) else (lambda ii: ii))(foi)
+                    for c in foidec:
+                        fw.write(c)
+                except IOError as e:
+                    if e.errno in (9, 32):
+                        sys.stderr.write('{0}\n'.format(str(e)))
+                        state['exit'] = 2
+                    else:
+                        raise
+                if raw_format:
+                    yield rcvd_msg_body
             elif (rm == 'GET') and raw_format:
                 query_string = (environ['QUERY_STRING'] if ('QUERY_STRING' in environ) else '')
                 if type_read:
@@ -159,8 +159,7 @@ def hcp_state(di_file=None, keep_listening=False, return_status=None, type_read=
                     oo = (pq[field_name][0] if ((field_name in pq) and (len(pq[field_name]) > 0)) else '')
                 else:
                     oo = query_string
-                with open_output_file_fd(di_file) as fw:
-                    fw.write(oo)
+                fw.write(oo)
     return (lambda k: state[k], handle_http_request)
 
 
@@ -190,7 +189,7 @@ def cph_state(di_file=None, type_read=None, raw_format=False, keep_listening=Fal
                 if not keep_listening:
                     state['done'] = True
                 try:
-                    return wsgiref.util.FileWrapper(os.fdopen(sys.stdin.fileno(), 'rb') if (di_file is None) else open(di_file, 'rb'))
+                    return wsgiref.util.FileWrapper(open_file_fd(di_file, is_writing=False))
                 finally:
                     pass
             else:
@@ -249,18 +248,18 @@ if __name__ == '__main__':
 
         if do_help:
             if run_name == http_ul_name:
-                help_msg = 'Usage: {0} [-p <port>] [-n <host>] [-f <output_file>] [-m <param_title>] [-s <status_line>] [-c] [-k] [-I]\n\n{tag_line}.\n\t<port>: port to listen with (default: {default_port})\n\t<host>: host to listen with (default: "{default_host}")\n\t<output_file>: path to output file (to be appended to)\n\t<param_title>: title of param to parse and return contents of (default is raw post-body contents in non-interactive mode, otherwise system-selected param); also, note that if the HTTP request does not set the POST Content-Type, the application/octet-stream a.k.a. raw post-body is used (even if the param-title is set); also, GET also works in non-interactive mode, using query-string as the post-body\n\t<status_line>: HTTP status line to use\n\t-c: CGI mode (requires a file name to be provided), standalone server is used if not specified\n\t-k: keep listening for additional requests after the file is requested (useful for clients that request files multiple times before fetching)\n\t-I: non-interactive mode (do not provide GUI for file upload, and if no <param_title> is provided then interpret post-data with application/octet-stream (the raw post-data itself is output, instead of a post param value)\n\n'
+                help_msg = 'Usage: {0} [-p <port>] [-n <host>] [-f <output_file>] [-m <param_title>] [-s <status_line>] [-c] [-k] [-I]\n\n{tag_line}.\n\t<port>: port to listen with (default: {default_port})\n\t<host>: host to listen with (default: "{default_host}")\n\t<output_file>: path to output file (to be appended to)\n\t<param_title>: title of param to parse and return contents of (default is raw post-body contents in non-interactive mode, otherwise system-selected param); also, note that if the HTTP request does not set the POST Content-Type, the application/octet-stream a.k.a. raw post-body is used (even if the param-title is set); also, GET also works in non-interactive mode, using query-string as the post-body\n\t<status_line>: HTTP status line to use\n\t-c: CGI mode (requires a file name to be provided), standalone server is used if not specified\n\t-k: keep listening for additional requests after the POST-data is received (useful for a quick add-POSTdatas-to-file server setup; ignored in CGI mode)\n\t-I: non-interactive mode (do not provide GUI for file upload, and if no <param_title> is provided then interpret post-data with application/octet-stream (the raw post-data itself is output, instead of a post param value)\n\n'
             elif run_name == http_dl_name:
-                help_msg = 'Usage: {0} [-p <port>] [-n <host>] [-f <input_file>] [-m <mime_type>] [-s <status_line>] [-c] [-k] [-I]\n\n{tag_line}.\n\t<port>: port to listen with (default: {default_port})\n\t<host>: host to listen with (default: "{default_host}")\n\t<input_file>: path to input file; the filename is sent to the client, where it is often saved with this name (if the file is not specified explicitly, an attempt is made to get the file name if specified on stdin (where this is supported); example "{prog_name} < file" (however, "cat file | {prog_name}" should not give it the file name)\n\t<status_line>: HTTP status line to use\n\t<mime_type>: mime content type to provide for file requests ("ext" to try to guess type based on file extension, if input file name is provided); default is "application/octet-stream"\n\t-k: keep listening for additional requests after the file is requested (useful for clients that request files multiple times before fetching)\n\t-I: view inline (prevents Content-Disposition header from being set to "attachment")\n\t-c: CGI mode (requires a file name to be provided), standalone server is used if not specified\n\n'
+                help_msg = 'Usage: {0} [-p <port>] [-n <host>] [-f <input_file>] [-m <mime_type>] [-s <status_line>] [-c] [-k] [-I]\n\n{tag_line}.\n\t<port>: port to listen with (default: {default_port})\n\t<host>: host to listen with (default: "{default_host}")\n\t<input_file>: path to input file; the filename is sent to the client, where it is often saved with this name (if the file is not specified explicitly, an attempt is made to get the file name if specified on stdin (where this is supported); example "{prog_name} < file" (however, "cat file | {prog_name}" should not give it the file name)\n\t<status_line>: HTTP status line to use\n\t<mime_type>: mime content type to provide for file requests ("ext" to try to guess type based on file extension, if input file name is provided); default is "application/octet-stream"\n\t-k: keep listening for additional requests after the file is requested (useful for clients that request files multiple times before fetching; ignored in CGI mode)\n\t-I: view inline (prevents Content-Disposition header from being set to "attachment")\n\t-c: CGI mode (requires a file name to be provided), standalone server is used if not specified\n\n'
             else:
                 help_msg = ''
             sys.stdout.write(help_msg.format(params[0], default_port=str(default_port), default_host=default_host, tag_line=prog_taglines[prog_descrs_map[run_name]], prog_name=run_name))
         else: 
-            state_kwparams = {'di_file': di_file, 'keep_listening': keep_listening, 'raw_format': raw_format, 'type_read': type_read, 'return_status': return_status}
+            state_kwparams = {'keep_listening': keep_listening, 'raw_format': raw_format, 'type_read': type_read, 'return_status': return_status}
             if (run_name == http_dl_name):
-                pass
+                state_kwparams['di_file'] = di_file
             elif (run_name == http_ul_name):
-                pass
+                state_kwparams['di_file'] = open_file_fd(di_file, is_writing=True)
             cp = (hcp_state if (run_name == http_ul_name) else (cph_state if (run_name == http_dl_name) else (lambda *p, **kp: None)))(**state_kwparams)
             if is_cgi:
                 if di_file is not None:
